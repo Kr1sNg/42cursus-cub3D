d893f0e824db552bf5ce58a335e6ff2c49d8a63b
Tat Hoang Nguyen <tat-nguy@c2r5p1.42perpignan.fr>
2025-06-10 15:37:06 +0200

    jun 10

--- cub3D/src/main/main.c
+++ cub3D/src/main/main.c
@@ -6,7 +6,7 @@
 /*   By: tat-nguy <tat-nguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 19:35:19 by tat-nguy          #+#    #+#             */
-/*   Updated: 2025/06/08 19:49:05 by tat-nguy         ###   ########.fr       */
+/*   Updated: 2025/06/10 15:34:00 by tat-nguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	main(int ac, char **av)
 	
 	if (ac != 2)
 		return (printf("Usage: ./cub3d valid_map.cub"));
-	ft_memset(&map, 0, sizeof(t_map));
+	
 	ft_parsing(av[1], &map);
 	
 	
